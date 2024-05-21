e4e84b73c69512d7fc45bd6125bb2403c05b005f
Caio Ramos de Lima <112398430+CaioRDL@users.noreply.github.com>
2024-05-21 20:25:46 -0300

    Add files via upload

---
+++ src/main/java/pr/senac/br/ola.java
@@ -0,0 +1,13 @@
+package pr.senac.br;
+
+public class ola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Ola Mundão");
+
+	}
+
+}
